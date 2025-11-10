7d258a23b1ede019dd1560112a474eee3b44f848
Nilsson-AvH <nlopez153@misena.edu.co>
2025-11-09 23:33:25 -0500

    Fin del programa 05-diezveces.js

---
+++ 05-diezveces.js
@@ -0,0 +1,17 @@
+//Escribe un programa que le pida al usuario ingresar una frase.
+// l programa debe imprimir la frase en la consola 10 veces.
+// NOTA: utiliza un ciclo para imprimir la frase las 10 veces.
+
+const prompt = require("prompt-sync")({sigint: true});
+
+function diezVeces (frase){
+
+    for(i=1;i<=10;i++){
+        console.log(`Repeticion No. ${i}: ${frase}`)
+    }
+
+}
+
+let laFrase = prompt(`Digite una frase, el convertidor la repetirá 10 veces: `)
+
+diezVeces(laFrase)
